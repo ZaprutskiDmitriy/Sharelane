699771041b3a9f46f70247b5a8acff9a09e82743
Dmitriy Zaprutski <zaprutsciy@mail.ru>
2021-07-20 22:50:31 +0300

    Tests

---
+++ src/test/java/com/sharelane/test/ZipCodeTest7.java
@@ -0,0 +1,30 @@
+package com.sharelane.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ZipCodeTest7 {
+    @Test
+    public void zipCodeNegative6(){
+        System.setProperty("webdriver.chrome.driver", "D:/TeachMeSkills/WebDriver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        // Open "Sharelane" login page
+        // Click on the "sign up" button
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+
+        // Fill the form with the required ZIP code
+
+        WebElement continueButton = driver.findElement(By.cssSelector("[type='submit'][value='Continue']"));
+        continueButton.click();
+
+        WebElement errorMessageElement = driver.findElement(By.className("error_message"));
+        String errrorMessage = errorMessageElement.getText();
+        Assert.assertEquals(errrorMessage, "Oops, error on page. ZIP code should have 5 digits");
+        //driver.quit();
+    }
+}
